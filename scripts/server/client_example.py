953fb7566e98c519797502b9a9b1c1e10bf7b6d4
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-10-25 13:37:46 +0100

    Handle empty lines

--- scripts/server/client_example.py
+++ scripts/server/client_example.py
@@ -9,6 +9,18 @@ import argparse
 from websocket import create_connection
 
 
+def add_empty_lines(text, positions):
+    output = []
+    indices = list(reversed(positions))
+    for i, line in enumerate(text.rstrip().split("\n")):
+        if indices:
+            while indices and indices[-1] == i:
+                output.append("")
+                indices.pop()
+        output.append(line)
+    return "\n".join(output)
+
+
 if __name__ == "__main__":
     # handle command-line options
     parser = argparse.ArgumentParser()
@@ -21,22 +33,32 @@ if __name__ == "__main__":
 
     count = 0
     batch = ""
+    empty_lines = []
     for line in sys.stdin:
+        text = line.decode('utf-8') if sys.version_info < (3, 0) else line
+        if not text.strip():
+            empty_lines.append(count)
+            continue
         count += 1
-        batch += line.decode('utf-8') if sys.version_info < (3, 0) else line
+        batch += text
         if count == args.batch_size:
             # translate the batch
             ws.send(batch)
             result = ws.recv()
+            if empty_lines:
+                result = add_empty_lines(result, empty_lines)
             print(result.rstrip())
 
             count = 0
             batch = ""
+            empty_lines = []
 
     if count:
         # translate the remaining sentences
         ws.send(batch)
         result = ws.recv()
+        if empty_lines:
+            result = add_empty_lines(result, empty_lines)
         print(result.rstrip())
 
     # close connection
